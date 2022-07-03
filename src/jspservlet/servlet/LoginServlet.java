24538d9b9cd53720eda2a297f432d27645af4fce
wiederholung <wiederholung@outlook.com>
2022-07-03 22:27:52 +0800

    success

--- src/jspservlet/servlet/LoginServlet.java
+++ src/jspservlet/servlet/LoginServlet.java
@@ -1,17 +1,16 @@
 package jspservlet.servlet;
 
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jspservlet.dao.UserDAO;
 import jspservlet.dao.impl.UserDAOImpl;
 import jspservlet.vo.User;
+
+import java.io.IOException;
 
 public class LoginServlet extends HttpServlet {
 	
---
+++ web/login.jsp
@@ -0,0 +1,15 @@
+<%@ page pageEncoding="GB18030"%>
+ 
+<html> 
+	<head>
+		<title>JSP for UserForm form</title>
+	</head>
+	<body>
+		<form method="post" action="./login"> 
+			username : <input type="text" name="username"/><br/>
+			password : <input type="text" name="password"/><br/>
+			<input type="SUBMIT" name="submit" value="Submit"> 
+		</form>
+	</body>
+</html>
+
