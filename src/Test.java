023ae6557e2194576eb4d03bc8e89ea6ae32dd8b
wiederholung <wiederholung@outlook.com>
2022-07-05 19:41:15 +0800

    test*2

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,7 @@ import model.vo.User;
 public class Test {
     public static void main(String[] args) {
         User user = new User();
-        user.setUsername("tom");
+        user.setUsername("HYT");
         user.setPassword("C 2998-mysql");
         UserDAO dao = new UserDAOImpl();
         int flag = dao.queryByUsername(user);
