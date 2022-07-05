d44cee31f9f50bb9b4b878149ed2b69115025c03
wiederholung <wiederholung@outlook.com>
2022-07-05 21:44:00 +0800

    tomcat9

--- src/control/LoginServlet.java
+++ src/control/LoginServlet.java
@@ -1,9 +1,9 @@
 package control;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import model.dao.UserDAO;
 import model.dao.impl.UserDAOImpl;
 import model.vo.User;
