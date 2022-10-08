d9674c359ecbcabd5c14f305a449ddc281b2e70c
wiederholung <wiederholung@outlook.com>
2022-10-08 19:25:22 +0800

    UserDAOImpl 通过测试

--- test/model/dao/impl/UserDAOImplTest.java
+++ test/model/dao/impl/UserDAOImplTest.java
@@ -2,73 +2,77 @@ package model.dao.impl;
 
 import model.dao.UserDAO;
 import model.vo.User;
-import model.vo.Warehouse;
 import org.junit.jupiter.api.Test;
 
-class UserDAOImplTest {
-    UserDAO userDAO = new UserDAOImpl();
+import java.util.ArrayList;
 
+class UserDAOImplTest {
+    private final UserDAO userDAO = new UserDAOImpl();
     private User m1 = new User("hyt", "C 2998-mysql",
             "Yitong Hu", true);
-    private User u1 = new User("wie", "123",
-            "Widerholung", false);
-    private Warehouse wh1 = new Warehouse("3rd", "Sport");
-
-
+    private User m2 = new User("admin", "password",
+            "HYT", true);
 
     @org.junit.jupiter.api.Test
-    void delWorkerFromWh() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void testDelWorkerFromWh() {
-    }
-
-    @Test
-    void getUser() {
+    void delWorkerFromWh() throws Exception {
+        userDAO.login(m1);
+        userDAO.delWorkerFromWh(m1, 19, 10);
     }
 
     @Test
-    void addUser() {
+    void getUser() throws Exception {
+        User u = userDAO.getUser(m1.getUsername());
+        System.out.println(u.getName());
     }
 
     @Test
-    void updateUser() {
+    void addUser() throws Exception {
+        User u = new User("mike3", "123456",
+                "Mike3", false);
+        userDAO.addUser(u);
     }
 
     @Test
-    void checkPasswordStrength() {
+    void checkPasswordStrength() throws Exception {
+        String password = "123";
+        System.out.println(userDAO.checkPasswordStrength(password));
     }
 
     @Test
-    void login() {
+    void login() throws Exception {
+        userDAO.login(m1);
+        System.out.println(m1.getHasWarehouse());
     }
 
     @Test
-    void registerWorker() {
+    void registerWorker() throws Exception {
+        User u2 = new User("mike4", "123456",
+                "Mike4", false);
+        userDAO.registerWorker(u2);
     }
 
     @Test
-    void addManger() {
+    void addManger() throws Exception {
+        ArrayList<Integer> whs = new ArrayList<>();
+        whs.add(1);
+        whs.add(2);
+        whs.add(3);
+        whs.add(10);
+        userDAO.addManger(m2, whs);
+        userDAO.login(m2);
+        System.out.println(m2.getHasWarehouse());
     }
 
     @Test
-    void getAccess() {
+    void getAccess() throws Exception {
+        User u = userDAO.getUser(m1.getUsername());
+        userDAO.getAccess(u);
+        System.out.println(u.getHasWarehouse());
     }
 
     @Test
-    void addWorkerToWh() {
-    }
-
-    @Test
-    void testUpdWorkerFromWh() {
-    }
-
-    @Test
-    void testDelWorkerFromWh1() {
-    }
-
-    @Test
-    void testDelWorkerFromWh2() {
+    void addWorkerToWh() throws Exception {
+        userDAO.login(m1);
+        userDAO.addWorkerToWh(m1, 19, 10);
     }
 }
